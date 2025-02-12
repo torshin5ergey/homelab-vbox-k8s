77354734f36c650953e9ef44fcf1fab4ddc7cdb7
torshin5ergey <ttorshin5ergey@gmail.com>
2025-02-13 01:09:02 +0300

    Fix staging status

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_info" {
-  description = "Node names and IPs"
-  value = [
-    for vm in virtualbox_vm.node : {
-      name = vm.name
-      ip   = vm.network_adapter[0].ipv4_address
-    }
-  ]
-}

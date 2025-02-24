a8cb154aa71f4e3b7d50c8ad5cc47e7930bb051e
torshin5ergey <ttorshin5ergey@gmail.com>
2025-02-24 20:36:46 +0300

    feat: add kubespray cluster installation

--- justinstall.sh
+++ justinstall.sh
@@ -3,7 +3,11 @@ set -e
 cd terraform/
 terraform init
 terraform apply -auto-approve
+cd ..
 
 # install k8s via kubespray
 git clone https://github.com/kubernetes-sigs/kubespray.git
-pip install -r kubespray/requirements.txt
+cd kubespray/
+pip install -r requirements.txt
+## installing the cluster
+ansible-playbook -i $HOME/homelab-vbox-k8s/ansible/inventory/inventory.ini cluster.yml -b -v

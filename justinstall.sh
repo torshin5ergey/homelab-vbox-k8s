6712dddc3b0928dff2d58bfe82493183250679b2
torshin5ergey <ttorshin5ergey@gmail.com>
2025-02-24 18:43:02 +0300

    feat: add kubespray dependencies installation to justinstall.sh

--- justinstall.sh
+++ justinstall.sh
@@ -3,3 +3,7 @@ set -e
 cd terraform/
 terraform init
 terraform apply -auto-approve
+
+# install k8s via kubespray
+git clone https://github.com/kubernetes-sigs/kubespray.git
+pip install -r kubespray/requirements.txt

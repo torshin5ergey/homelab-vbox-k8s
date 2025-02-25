e007dff1ba323f2cb79c9b43604fe3f1ffb80360
torshin5ergey <ttorshin5ergey@gmail.com>
2025-02-25 18:44:28 +0300

    feat: add kubeconfig fetch step to justinstall.sh

--- justinstall.sh
+++ justinstall.sh
@@ -11,3 +11,6 @@ cd kubespray/
 pip install -r requirements.txt
 ## installing the cluster
 ansible-playbook -i $HOME/homelab-vbox-k8s/ansible/inventory/inventory.ini cluster.yml -b -v
+### Get kubeconfig
+cd ..
+ansible-playbook -i ansible/inventory/inventory.ini ansible/playbooks/02_get_kubeconfig.yaml

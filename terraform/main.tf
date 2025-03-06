c99524e01b1a5e3a5900bbe4ae9ef0ce0758dba1
torshin5ergey <ttorshin5ergey@gmail.com>
2025-03-06 16:57:05 +0300

    refactor(terraform): delete all commented lines

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,48 +9,4 @@ resource "virtualbox_vm" "node" {
     type           = "bridged"
     host_interface = "wlp0s20f3"
   }
-
-  # provisioner "file" {
-  #   content     = local_file.ssh_public.content
-  #   destination = "/home/${var.username}/.ssh/authorized_keys"
-
-  #   connection {
-  #     type     = "ssh"
-  #     user     = var.username
-  #     password = "vagrant"
-  #     host     = self.network_adapter.0.ipv4_address
-  #   }
-  # }
-
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "chmod 700 ~/.ssh",
-  #     "chmod 600 ~/.ssh/authorized_keys",
-  #     "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config",
-  #     "sudo systemctl restart sshd",
-  #   ]
-
-  #   connection {
-  #     type        = "ssh"
-  #     user        = var.username
-  #     private_key = tls_private_key.cluster_ssh.private_key_openssh
-  #     host        = self.network_adapter[0].ipv4_address
-  #   }
-  # }
 }
-
-# resource "tls_private_key" "cluster_ssh" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
-
-# resource "local_file" "ssh_private" {
-#   filename        = "${path.module}/.ssh/id_rsa"
-#   content         = tls_private_key.cluster_ssh.private_key_openssh
-#   file_permission = "0600"
-# }
-
-# resource "local_file" "ssh_public" {
-#   filename = "${path.module}/.ssh/id_rsa.pub"
-#   content  = tls_private_key.cluster_ssh.public_key_openssh
-# }

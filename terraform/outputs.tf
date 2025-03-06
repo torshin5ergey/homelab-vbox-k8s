64158d9264bd4f49b9e0e0e89d6b5b81fce78b4a
torshin5ergey <ttorshin5ergey@gmail.com>
2025-03-06 17:08:15 +0300

    refactor(terraform): restructure vm_info ouptut to group by roles (master/worker)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,16 @@
 # node-name: node-ip
 output "vm_info" {
-  description = "Node names and IPs"
+  description = "Node names and IPs by roles"
   value = {
-    for vm in virtualbox_vm.node : vm.name => vm.network_adapter[0].ipv4_address
+    masters = {
+      for vm in virtualbox_vm.node :
+      vm.name => vm.network_adapter[0].ipv4_address
+      if can(regex("master", vm.name))
+    }
+    workers = {
+      for vm in virtualbox_vm.node :
+      vm.name => vm.network_adapter[0].ipv4_address
+      if can(regex("worker", vm.name))
+    }
   }
 }
